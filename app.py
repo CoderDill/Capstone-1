a4f3751792bb0168a7129ffce9e276f2213b7cb5
Matthew Dillon <mddillon47@gmail.com>
2021-10-10 15:34:14 -0700

    api_key fix

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, request, render_template, redirect, flash, session, jso
 from flask_debugtoolbar import DebugToolbarExtension
 import requests
 from models import db, connect_db, User, Bet
-
 from forms import UserSignInForm, UserSignUpForm, AddBetForm
 from sqlalchemy.exc import IntegrityError
 
+API_KEY = '462e5708d7msh6be12143d24c056p1a05b0jsn6f202208f183'
 
 CURR_USER_KEY = "curr_user"
 
