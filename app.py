857d9c27585576dbb73c4e9ab3a31dcea2e6e6f1
CrappyDill <mddillon47@yahoo.com>
2021-10-16 08:59:50 -0700

    secret update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template, redirect, flash, session, jsonify, g
 from flask_debugtoolbar import DebugToolbarExtension
 import requests
+from secret import API_KEY
 from models import db, connect_db, User, Bet
 from forms import UserSignInForm, UserSignUpForm, AddBetForm
 from sqlalchemy.exc import IntegrityError
 
-API_KEY = '462e5708d7msh6be12143d24c056p1a05b0jsn6f202208f183'
 
 CURR_USER_KEY = "curr_user"
 
