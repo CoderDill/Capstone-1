4a946043efdd600247ffb9f9e45785ed4a82e476
CrappyDill <mddillon47@gmail.com>
2021-10-21 17:20:24 -0700

    pos_win

--- models.py
+++ models.py
@@ -47,6 +47,7 @@ class Bet(db.Model):
     team_1 = db.Column(db.String(20), nullable=False)
     team_2 = db.Column(db.String(20), nullable=False)
     amt_wagered = db.Column(db.Float, nullable=False)
+    pos_win = db.Column(db.Float, nullable=False)
     result = db.Column(db.String(10), nullable=False, default="pending")
     amt_paid = db.Column(db.Float, nullable=False, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
